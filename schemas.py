89e25ea49da48888480524c55a39e7c8320a9f7c
Siddhant Bisen <145115589+sid5354535457N@users.noreply.github.com>
2025-01-23 23:15:03 +0530

    Delete schemas.py

--- schemas.py
+++
@@ -1,9 +0,0 @@
-from pydantic import BaseModel
-
-class PredictionInput(BaseModel):
-    Temperature: float
-    Run_Time: float
-
-class PredictionResponse(BaseModel):
-    Downtime: str
-    Confidence: float
